71378ac965df972b82f7d7be5d46afa0aeb2154d
Amir Ali Taheri <amiralitaheri2000th@gmail.com>
2022-03-30 17:26:02 +0430

    show & sync cost & income

--- src/js/app.js
+++ src/js/app.js
@@ -19,16 +19,40 @@ document.addEventListener("alpine:init", () => {
         description: "",
       },
 
-      deleteCostItem(index) {
+      costCount: this.$persist(0),
+      incomeCount: this.$persist(0),
+      costArray: [],
+
+      deleteCostItem(data, index) {
         this.formData = this.formData.filter((data, dataIndex) => {
           return index !== dataIndex;
         });
+
+        console.log(data.cost.typeOfCost);
+
+        if (data.cost.typeOfCost == "درآمد") {
+          this.incomeCount -= data.cost.price;
+          return;
+        }
+
+        if (data.cost.typeOfCost == "هزینه") {
+          this.costCount -= data.cost.price;
+          return;
+        }
       },
 
       formData: this.$persist([]),
       storeDataToLocalStorage() {
         this.formData.push({
           cost: this.cost,
+        });
+
+        this.costArray = this.formData.filter((formData, dataIndex) => {
+          return formData.cost.typeOfCost == "درآمد";
+        });
+
+        this.costArray.forEach((item) => {
+          this.incomeCount += item.cost.price;
         });
 
         this.cost = {
